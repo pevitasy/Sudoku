4c4575ed0c4fb2622b3f31191e6b22b9cf762877
pevitasy <142603043+pevitasy@users.noreply.github.com>
2023-12-19 18:29:41 +0700

    Create CellResources.java

---
+++ CellResources.java
@@ -0,0 +1,33 @@
+package Sudoku;
+
+/**
+ * ES234317-Algorithm and Data Structures
+ * Semester Ganjil, 2023/2024
+ * Group Capstone Project
+ * Group #5
+ * 1 - 5026221032 - Fadillah Nur Laili
+ * 2 - 5026221171 - Muhammad Rafi Novyansyah
+ * 3 - 5026221202 - Akbar Daniswara Cahya Buana
+ */
+
+import java.awt.*;
+
+public class CellResources {
+    public static final Color BG_GIVEN = new Color(224, 234, 13); // RGB
+    public static final Color BG_GIVEN_2 = new Color(224, 234, 13);
+    public static final Color FG_GIVEN = Color.BLACK;
+    public static final Color FG_NOT_GIVEN = Color.BLACK;
+    public static final Color BG_TO_GUESS  = new Color(224,234,13);
+    public static final Color BG_TO_GUESS_2 = new Color(224, 234, 13);
+
+    public static final Color BG_GIVEN_DARK = new Color(0, 5, 14);
+    public static final Color BG_GIVEN_2_DARK = new Color(0, 5, 14); // RGB
+    public static final Color FG_GIVEN_DARK = Color.WHITE;
+    public static final Color FG_NOT_GIVEN_DARK = Color.WHITE;
+    public static final Color BG_TO_GUESS_DARK  =  new Color(0, 5, 14);
+    public static final Color BG_TO_GUESS_2_DARK =new Color(0, 5, 14);
+
+    public static final Color BG_CORRECT_GUESS = new Color(0, 216, 0);
+    public static final Color BG_WRONG_GUESS   = new Color(216, 0, 0);
+    public static final Font FONT_NUMBERS = new Font("Calibri", Font.PLAIN, 28);
+}
