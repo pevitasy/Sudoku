1f6444972e267fdb3a2f318299f63249684f2fbf
pevitasy <142603043+pevitasy@users.noreply.github.com>
2023-12-19 18:34:42 +0700

    Create SudokuConstants.java

---
+++ SudokuConstants.java
@@ -0,0 +1,19 @@
+package Sudoku;
+
+/**
+ * ES234317-Algorithm and Data Structures
+ * Semester Ganjil, 2023/2024
+ * Group Capstone Project
+ * Group #5
+ * 1 - 5026221032 - Fadillah Nur Laili
+ * 2 - 5026221171 - Muhammad Rafi Novyansyah
+ * 3 - 5026221202 - Akbar Daniswara Cahya Buana
+ */
+
+
+public class SudokuConstants {
+    /** Size of the board */
+    public static final int GRID_SIZE = 9;
+    /** Size of sub-grid of the board */
+    public static final int SUBGRID_SIZE = 3;
+}
