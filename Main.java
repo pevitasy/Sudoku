81bcf86d7bb8401c33cd8e7944ba1f14fec2f0f1
pevitasy <142603043+pevitasy@users.noreply.github.com>
2023-12-19 18:32:50 +0700

    Create Main.java

---
+++ Main.java
@@ -0,0 +1,20 @@
+package Sudoku;
+/**
+ * ES234317-Algorithm and Data Structures
+ * Semester Ganjil, 2023/2024
+ * Group Capstone Project
+ * Group #5
+ * 1 - 5026221032 - Fadillah Nur Laili
+ * 2 - 5026221171 - Muhammad Rafi Novyansyah
+ * 3 - 5026221202 - Akbar Daniswara Cahya Buana
+ */
+public class Main {
+    public static void main(String[] args) {
+        javax.swing.SwingUtilities.invokeLater(new Runnable() {
+            @Override
+            public void run() {
+                new SudokuMain();
+            }
+        });
+    }
+}
