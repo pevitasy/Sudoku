9087ac9d0e4f9aab3cf5b2941a276d5901a31682
pevitasy <142603043+pevitasy@users.noreply.github.com>
2023-12-19 18:35:39 +0700

    Create ToolBarResources.java

---
+++ ToolBarResources.java
@@ -0,0 +1,20 @@
+package Sudoku;
+
+/**
+ * ES234317-Algorithm and Data Structures
+ * Semester Ganjil, 2023/2024
+ * Group Capstone Project
+ * Group #5
+ * 1 - 5026221032 - Fadillah Nur Laili
+ * 2 - 5026221171 - Muhammad Rafi Novyansyah
+ * 3 - 5026221202 - Akbar Daniswara Cahya Buana
+ * */
+
+
+import java.awt.*;
+
+public class ToolBarResources {
+    public static final Color BG_TOOLBAR = new Color(247, 251, 252);
+    public static final Color FG_TOOLBAR = new Color(12, 12, 12);
+    public static final Font FONT_TOOLBAR = new Font("Calibri", Font.PLAIN, 15);
+}
