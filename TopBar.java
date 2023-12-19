e2e644e2c3bb06dd4857a487cb330ece4431d620
pevitasy <142603043+pevitasy@users.noreply.github.com>
2023-12-19 18:37:32 +0700

    Create TopBar.java

---
+++ TopBar.java
@@ -0,0 +1,16 @@
+package Sudoku;
+/**
+ * ES234317-Algorithm and Data Structures
+ * Semester Ganjil, 2023/2024
+ * Group Capstone Project
+ * Group #5
+ * 1 - 5026221032 - Fadillah Nur Laili
+ * 2 - 5026221171 - Muhammad Rafi Novyansyah
+ * 3 - 5026221202 - Akbar Daniswara Cahya Buana
+ */
+
+import javax.swing.*;
+
+public class TopBar extends JPanel {
+
+}
